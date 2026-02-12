5e4d2778f36c0a9e00f8b4d4fe2d2ae2d99f0b1f
MatthewJones <mjones2761@gmail.com>
2026-02-12 15:55:14 -0500

    2nd try

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Simple Todo App
+
 This Project/app is meant to make a project deadline board, now lets go more in depth in what I mean by that. This app is meant to help you keep yourself to certain deadlines and gently remind you of what you have to get done for the day or week. All you have to do is simply add your List of things todo for as far out as you wish and check them off as you go.
 
 ## Functionality
 
-This app allows you to add as many items you need to get done as you want to help you keep track of what needs to get accomplished. Some people use a calendar and then what ever is on it for that day they add that to there TODO board and check it off one by one. People find it easier to do this as it gives them a sense of accomplishment when they check something off and helps their day go by faster. 
+This app allows you to add as many items you need to get done as you want to help you keep track of what needs to get accomplished. Some people use a calendar and then what ever is on it for that day they add that to there TODO board and check it off one by one. People find it easier to do this as it gives them a sense of accomplishment when they check something off and helps their day go by faster.
 
 ## Technology
-In this app we have used mostly java script to program how this works, now sometimes people use just the one Java Script file and do everything that way but as seen here............. We broke it up into several to make it easier to edit the backend code to change or alter  the way the website works. 
 
+In this app we have used mostly java script to program how this works, now sometimes people use just the one Java Script file and do everything that way but as seen here............. We broke it up into several to make it easier to edit the backend code to change or alter the way the website works.
 
 ## Deployment
 
 This project is deployed on GitHub Pages:
 
-- ADD LINK HERE
+- [ADD LINK HERE](https://mjones2761.github.io/se_project_todo-app_/)
--- components/Todo.js
+++ components/Todo.js
@@ -4,51 +4,58 @@ class Todo {
     this._templateElement = document.querySelector(selector);
   }
 
-_setEventListeners() {
- const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
-  todoDeleteBtn.addEventListener("click", () => {
-    this._todoElement.remove();
-  });
+  _setEventListeners() {
+    this._deleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
 
-  this._todoCheckboxEl.addEventListener("change", () => {
-    this._data.completed = !this._data.completed;
-    console.log(this._data.completed);
-  });
-}
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = this._todoCheckboxEl.checked;
+    });
+  }
 
-_generateCheckboxEl() {
-   this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-  this._todoLabel = this._todoElement.querySelector(".todo__label");
-  this._todoCheckboxEl.checked = this._data.completed;
-  this._todoCheckboxEl.id = `todo-${this._data.id}`;
-  this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
-}
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
 
   getView() {
     this._todoElement = this._templateElement.content
-    .querySelector(".todo")
-    .cloneNode(true);
+      .querySelector(".todo")
+      .cloneNode(true);
 
-  const todoNameEl = this._todoElement.querySelector(".todo__name");
-  const todoDate = this._todoElement.querySelector(".todo__date");
-  const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    
+    this._nameEl = this._todoElement.querySelector(".todo__name");
+    this._dateEl = this._todoElement.querySelector(".todo__date");
+    this._deleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
-  todoNameEl.textContent = this._data.name;
-  
-  todoDate.textContent = new Date(this._data.date).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+    this._nameEl.textContent = this._data.name;
 
+    if (
+      this._data.date &&
+      !isNaN(new Date(this._data.date).getTime())
+    ) {
+      this._dateEl.textContent = new Date(this._data.date).toLocaleDateString(
+        "en-US",
+        {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }
+      );
+    } else {
+      this._dateEl.textContent = "";
+    }
 
-this._generateCheckboxEl();
-this._setEventListeners();
+    this._generateCheckboxEl();
+    this._setEventListeners();
 
-  const dueDate = new Date(this._data.date);
-
-  return this._todoElement;
+    return this._todoElement;
   }
-  }
+}
 
-  export default Todo;
+export default Todo;
